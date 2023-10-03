2bba136e99b039cfc22bea93cf84c496eb1cac86
JunbongJang <jangjunbong@gmail.com>
2023-10-03 23:54:09 +0900

    fixed google analytics (my adblock was cause)

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./lib/Router";
-import ReactGA from "react-ga4";
+// import ReactGA from "react-ga4";
 
 import { useLocation } from 'react-router-dom';
 
 function App() {
-  ReactGA.initialize("G-XYP72XXDTN");
-  ReactGA.send({ hitType: "pageview", page: "/", title: "JunbongJang's React Website" });
+  // ReactGA.initialize("G-XYP72XXDTN");
+  // ReactGA.send({ hitType: "pageview", page: "/", title: "JunbongJang's React Website" });
 
   return (
     <ThemeProvider theme={defaultTheme}>
