054c0bf861f9bbef84157035fbecc24b2a68eaf2
JunbongJang <jangjunbong@gmail.com>
2023-10-03 15:14:27 +0900

    first react website

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./lib/Router";
+import ReactGA from "react-ga4";
+
 
 function App() {
+  ReactGA.initialize("G-XYP72XXDTN");
+  ReactGA.send({ hitType: "homepage", page: "/", title: "JunbongJang's React Website" });
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
