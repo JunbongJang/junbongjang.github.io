738da3835c1bb116ccac987c8c4ec1247c756e42
JunbongJang <jangjunbong@gmail.com>
2023-11-12 21:02:42 +0900

    deploy nextjs to github

--- next.config.js
+++ next.config.js
@@ -1,16 +1,29 @@
+// const nextConfig = {
+//     images: {
+//         unoptimized: true
+//     }
+
+// // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+// // trailingSlash: true,
+
+// // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+// // skipTrailingSlashRedirect: true,
+
+// // Optional: Change the output directory `out` -> `dist`
+// // distDir: 'dist',
+// }
+   
+// module.exports = nextConfig
+
+/**
+* @type {import('next').NextConfig}
+*/
 const nextConfig = {
     images: {
-        unoptimized: true
-    }
-
-// Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-// trailingSlash: true,
-
-// Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-// skipTrailingSlashRedirect: true,
-
-// Optional: Change the output directory `out` -> `dist`
-// distDir: 'dist',
-}
-   
-module.exports = nextConfig
+      loader: 'akamai',
+      path: '',
+    },
+    assetPrefix: './',
+  };
+  
+  export default nextConfig;
