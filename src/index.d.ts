8d59b8fa724aca439fd08fd9e603475f3016e58f
JunbongJang <jangjunbong@gmail.com>
2023-11-12 20:42:36 +0900

    made nextjs version of my website

--- src/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '*.jpg';
-declare module '*.svg';
-declare module '*.png';
-declare module "*.ico";
-declare module "*.css";
--- src/utils/formatText.ts
+++
@@ -1,8 +0,0 @@
-export function formatText(text: string, limitLength = 50) {
-  const textArr = text.split(" ")
-  const newText = textArr.map((string, index) => {
-    if (index < limitLength) return string
-  }).filter(string => string !== undefined
-  )
-  return `${newText.toString().replaceAll(",", " ")}...`
-}
