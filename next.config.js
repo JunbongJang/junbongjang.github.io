267d448bb26b3988047ec79ce8810e34bc5a0a7c
JunbongJang <jangjunbong@gmail.com>
2023-11-12 22:34:22 +0900

    update

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@
 //         unoptimized: true
 //     }
 
-// // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-// // trailingSlash: true,
-
-// // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-// // skipTrailingSlashRedirect: true,
-
-// // Optional: Change the output directory `out` -> `dist`
-// // distDir: 'dist',
 // }
    
 // module.exports = nextConfig
@@ -19,6 +11,20 @@
 * @type {import('next').NextConfig}
 */
 const nextConfig = {
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
+    }, 
     images: {
       loader: 'akamai',
       path: '',
