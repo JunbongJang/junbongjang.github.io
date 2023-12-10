c049411cd3e45de11dfe20a9b82b43a4fc5257bb
JunbongJang <jangjunbong@gmail.com>
2023-12-10 10:26:49 +0900

    change id.js

--- pages/blogs/[id].js
+++ pages/blogs/[id].js
@@ -4,34 +4,35 @@ import Link from 'next/link';
 import Date from '../../components/date';
 import styles from '../../styles/blog_detail.module.css';
 import { Fragment } from 'react';
+import { useState, useEffect } from 'react'
 
-// import bookmarkPlugin from "@notion-render/bookmark-plugin";
-// import { NotionRenderer } from "@notion-render/client";
-// import hljsPlugin from "@notion-render/hljs-plugin";
-// import { notFound } from "next/navigation";
-// import { Render } from '@9gustin/react-notion-render'
 import { renderBlock } from '../../components/notion_renderer';
-
 
 export default function BlogPage({ blog_data }) {
   // console.log('page_response', blog_data.page_response)
   // console.log('list_of_blocks', ...blog_data.list_of_blocks);
   // console.log(blog_data.list_of_blocks.length);
 
-  // const blocks = fetchPageBlocks(blog_data.page_response.id);
+  const [updated_blog_data, setBlogData] = useState(blog_data)
+  console.log('updated_blog_data', updated_blog_data)
+  
+  // const [isLoading, setLoading] = useState(true)
 
-  // const renderer = new NotionRenderer({
-  //   client: notion,
-  // });
+  useEffect(() => {
+      async function fetchAndSetBlogData() {
+        const data = await getBlogData( blog_data.blog_id );
+        setBlogData(data);
+      }
+      fetchAndSetBlogData();
+    }, []);
+ 
 
-  // renderer.use(hljsPlugin());
-  // renderer.use(bookmarkPlugin());
 
   return (
     <>
         
         <Head>
-            <title>Blog - {blog_data.page_response.properties.이름.title[0].plain_text}</title>
+            <title>Blog - {updated_blog_data.page_response.properties.이름.title[0].plain_text}</title>
             <meta name="description" content="junbong jang's blog" />
         </Head>
         
@@ -50,7 +51,7 @@ export default function BlogPage({ blog_data }) {
           </header>
           <div>
             
-            <h1>{blog_data.page_response.properties.이름.title[0].plain_text}</h1>
+            <h1>{updated_blog_data.page_response.properties.이름.title[0].plain_text}</h1>
             
           </div>
           <footer>
@@ -58,7 +59,7 @@ export default function BlogPage({ blog_data }) {
               <i className="fas fa-solid fa-calendar"></i>
               
               {/* <small>Last Edited Date: <Date dateString={blog_data.page_response.last_edited_time} /> </small> */}
-              <small>Created Date: <Date dateString={blog_data.page_response.created_time} /></small>
+              <small>Created Date: <Date dateString={updated_blog_data.page_response.created_time} /></small>
             </span>
             {/* <span>
               <i className="fas fa-solid fa-comment"></i>
@@ -104,38 +105,48 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
     // Fetch necessary data for the blog post using params.id
-    
-    const notion = new Client({ auth: process.env.NOTION_API_KEY });
-    const response_blog = await notion.blocks.children.list({
-        block_id: params.id,
-    });
+  
+    const blog_data = await getBlogData( params.id );
 
-
-    let total_response_blog;
-    if (response_blog.has_more) {  // If the response exceeds the 100 block limit of notion api 
-
-      const response_blog2 = await notion.blocks.children.list({
-        block_id: params.id,
-        start_cursor: response_blog.next_cursor
-      });
-
-      total_response_blog = response_blog.results.concat(response_blog2.results);
-    } 
-    else {
-      total_response_blog = response_blog.results;
-    }
-
-
-    const page_response = await notion.pages.retrieve({ page_id: params.id });
-
-    const blog_data = {
-      list_of_blocks: total_response_blog,
-      page_response: page_response
-    }
     return {
         props: {
             blog_data
         },
+        revalidate: 1, // In seconds
     };
 
+}
+
+async function getBlogData( blog_id ) {
+  // Fetch necessary data for the blog post using params.id
+    
+  const notion = new Client({ auth: process.env.NOTION_API_KEY });
+  const response_blog = await notion.blocks.children.list({
+      block_id: blog_id,
+  });
+
+
+  let total_response_blog;
+  if (response_blog.has_more) {  // If the response exceeds the 100 block limit of notion api 
+
+    const response_blog2 = await notion.blocks.children.list({
+      block_id: blog_id,
+      start_cursor: response_blog.next_cursor
+    });
+
+    total_response_blog = response_blog.results.concat(response_blog2.results);
+  } 
+  else {
+    total_response_blog = response_blog.results;
+  }
+
+
+  const page_response = await notion.pages.retrieve({ page_id: blog_id });
+
+  const blog_data = {
+    list_of_blocks: total_response_blog,
+    page_response: page_response,
+    blog_id: blog_id
+  }
+  return blog_data;
 }
