104dcb6ea3289179352b5f6125ef514a675cd7bd
JunbongJang <jangjunbong@gmail.com>
2023-12-10 10:27:42 +0900

    react backup

---
+++ .next/server/chunks/155.js
@@ -0,0 +1,18 @@
+"use strict";exports.id=155,exports.ids=[155],exports.modules={3155:(e,t,s)=>{s.r(t),s.d(t,{default:()=>c});var n=s(997),a=s(6859),r=s.n(a),o=s(4298),i=s.n(o);let MyDocument=class MyDocument extends r(){render(){return(0,n.jsxs)(a.Html,{lang:"en",children:[(0,n.jsxs)(a.Head,{children:[n.jsx("meta",{charSet:"utf-8"}),n.jsx("meta",{name:"description",content:"Junbong's personal website introducing about himself, his work and research interests"}),n.jsx("meta",{name:"author",content:"Junbong Jang"}),n.jsx("meta",{property:"og:site_name",content:"Junbong Jang"}),n.jsx("meta",{property:"og:site",content:""})," ",n.jsx("meta",{property:"og:title",content:""})," ",n.jsx("meta",{property:"og:description",content:""})," ",n.jsx("meta",{property:"og:image",content:""})," ",n.jsx("meta",{property:"og:url",content:""})," ",n.jsx("meta",{name:"twitter:card",content:"summary_large_image"})," ",n.jsx("link",{rel:"preconnect",href:"https://fonts.gstatic.com"}),n.jsx("link",{href:"https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&family=Poppins:wght@600&display=swap",rel:"stylesheet"}),n.jsx("link",{rel:"stylesheet",href:"https://cdn.jsdelivr.net/gh/jpswalsh/academicons@1/css/academicons.min.css"}),n.jsx("link",{href:"https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap",rel:"stylesheet"}),n.jsx("link",{rel:"icon",href:"images/favicon.ico"})]}),(0,n.jsxs)("body",{children:[n.jsx(i(),{src:"https://use.fontawesome.com/fb615be7a2.js"}),n.jsx(i(),{src:"https://kit.fontawesome.com/348d8fb546.js",crossOrigin:"anonymous"}),n.jsx(i(),{async:!0,src:"https://www.googletagmanager.com/gtag/js?id=G-XYP72XXDTN"}),n.jsx(i(),{strategy:"afterInteractive",dangerouslySetInnerHTML:{__html:`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-XYP72XXDTN');
+                `}}),n.jsx(i(),{strategy:"afterInteractive",dangerouslySetInnerHTML:{__html:`
+            (function(l) {
+                if (l.search[1] === '/' ) {
+                var decoded = l.search.slice(1).split('&').map(function(s) { 
+                    return s.replace(/~and~/g, '&')
+                }).join('?');
+                window.history.replaceState(null, null,
+                    l.pathname.slice(0, -1) + decoded + l.hash
+                );
+                }
+            }(window.location))
+            `}}),n.jsx(a.Main,{}),n.jsx(a.NextScript,{}),n.jsx(i(),{src:"/js/jquery.min.js",strategy:"beforeInteractive"})," ",n.jsx(i(),{src:"/js/jquery.easing.min.js",strategy:"beforeInteractive"})," ",n.jsx(i(),{src:"/js/bootstrap.min.js",strategy:"afterInteractive"})," ",n.jsx(i(),{src:"/js/scripts.js",strategy:"afterInteractive"}),"  "]})]})}};let c=MyDocument}};
